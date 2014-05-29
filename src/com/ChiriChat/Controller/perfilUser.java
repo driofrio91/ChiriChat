fa0cec7edb8210503802c3b9786d2d03bf4770d6
danny <superdanny.91@gmail.com>
2014-05-29 03:24:39 +0200

    Layout Perfil terminado,  falta la actividad

---
+++ src/com/ChiriChat/Controller/perfilUser.java
@@ -0,0 +1,14 @@
+package com.ChiriChat.Controller;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by danny on 29/05/14.
+ */
+public class perfilUser extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+}
