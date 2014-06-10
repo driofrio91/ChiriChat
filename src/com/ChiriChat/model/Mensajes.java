3093f30e6a61e8263c9686197a08627fad44d334
Danny <superdanny.91@gmail.com>
2014-06-10 09:37:00 +0200

    Clases con licencia, implementacion del servicio en proceso

--- src/com/ChiriChat/model/Mensajes.java
+++ src/com/ChiriChat/model/Mensajes.java
@@ -1,15 +1,29 @@
-package com.ChiriChat.model;/**
- * Created by danny on 13/05/2014.
- */
+/*
+* Copyright (C) 2014 Alejandro Moreno Jimenez | alejandroooo887@gmail.com
+*					 Danny Riofrio Jimenez | superdanny.91@gmail.com
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+*/
+
+
+package com.ChiriChat.model;
 
 import android.os.Parcel;
 import android.os.Parcelable;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-/**
- * @author Danny Riofrio Jimenez
- */
+
 public class Mensajes implements Parcelable{
 
     private int idMensaje;
